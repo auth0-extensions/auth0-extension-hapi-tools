727c48cc699569ee7437df42f8eb96a6a8c5f295
Sandrino Di Mattia <contact@sandrino.be>
2016-11-07 08:53:36 -0800

    Update dependencies

--- src/server.js
+++ src/server.js
@@ -1,17 +1,7 @@
-const configProvider = require('auth0-extension-tools').configProvider;
-const Webtask = require('webtask-tools');
+const tools = require('auth0-extension-tools');
 
 module.exports.createServer = function(cb) {
-  var server = null;
-
-  return fromHapi(function hapiFactory(webtaskContext) {
-    if (!server) {
-      const config = configProvider.fromWebtaskContext(webtaskContext);
-      server = cb(config, webtaskContext.storage);
-    }
-
-    return server;
-  });
+  return fromHapi(tools.createServer(cb));
 };
 
 const SANITIZE_RX = /[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g;
